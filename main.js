5697ed3337e15f2bfa35fbff6a3813e7e67952f1
francesca <fradeluca85@gmail.com>
2021-11-08 16:08:12 +0100

    hamurgermenu

---
+++ main.js
@@ -0,0 +1,10 @@
+// alert ("hello world")
+
+// aprire hamburgerMenu
+const hamburgerMenuOpen = document.querySelector('.header-right');
+
+hamburgerMenuOpen.addEventListener('click', function(){
+    document.querySelector('.hamburger-menu').classList.add('active')
+    }
+);
+
